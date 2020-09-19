68a8f620666ca734859972d3dbccc94deb128e51
anhv <anhv.ict91@gmail.com>
2020-09-19 10:50:20 +0700

    update

--- script.py
+++ script.py
@@ -1,17 +1,30 @@
 from underthesea import word_tokenize
 
+
+def load_dictionary():
+    words = open("data/dictionary.txt")
+    return words
+
+
+dictionary = load_dictionary()
+
+
 def load_raw_sentences():
     sentences = open("data/sentences.txt").read().splitlines()
     return sentences
 
+
 def convert_to_tokenize(tokens):
     return "\n".join(tokens)
 
+
 sentences = load_raw_sentences()
+
 
 def generate_tokenize(sentence):
     tokens = word_tokenize(sentence)
     return convert_to_tokenize(tokens)
+
 
 tokenizes = [generate_tokenize(sentence) for sentence in sentences]
 
