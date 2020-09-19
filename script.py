c3a564b227f52f0747f7a7aa770cf54c67e2fa00
anhv <anhv.ict91@gmail.com>
2020-09-19 10:48:17 +0700

    update

---
+++ script.py
@@ -0,0 +1,19 @@
+from underthesea import word_tokenize
+
+def load_raw_sentences():
+    sentences = open("data/sentences.txt").read().splitlines()
+    return sentences
+
+def convert_to_tokenize(tokens):
+    return "\n".join(tokens)
+
+sentences = load_raw_sentences()
+
+def generate_tokenize(sentence):
+    tokens = word_tokenize(sentence)
+    return convert_to_tokenize(tokens)
+
+tokenizes = [generate_tokenize(sentence) for sentence in sentences]
+
+content = "\n\n".join(tokenizes)
+open("tmp/tokenize_data.txt", "w").write(content)
