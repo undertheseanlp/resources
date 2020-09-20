998f10b56d821c2c4485f8c449c3b60bf02e811b
anhv <anhv.ict91@gmail.com>
2020-09-20 15:20:19 +0700

    upgrade

--- data.py
+++ data.py
@@ -1,11 +1,34 @@
 import sqlalchemy
-
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 engine = sqlalchemy.create_engine('sqlite:///data/dictionary.sqlite')
+Base = declarative_base()
 connection = engine.connect()
 
 metadata = sqlalchemy.MetaData()
 
-words = sqlalchemy.Table('words', metadata, autoload=True, autoload_with=engine)
 
-entities = sqlalchemy.Table('entities', metadata, autoload=True, autoload_with=engine)
+class Word(Base):
+    __tablename__ = 'words'
+
+    word = Column('word', String, primary_key=True)
+
+class Entity(Base):
+    __tablename__ = 'entities'
+
+    id = Column('id', Integer, primary_key=True)
+    name = Column('name', String)
+    tag = Column('tag', String)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+words = session.query(Word)
+entities = session.query(Entity)
+print(0)
+
+# words = sqlalchemy.Table('words', metadata, autoload=True, autoload_with=engine)
+#
+# entities = sqlalchemy.Table('entities', metadata, autoload=True, autoload_with=engine)
