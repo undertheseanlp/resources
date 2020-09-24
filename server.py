228aa736d998f4472c98d92d7ce6e08cd22161a8
Vu Anh <anhv.ict91@gmail.com>
2020-09-24 08:58:25 +0700

    update

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__, static_url_path='/', static_folder='app')
+
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+
+@app.route('/amr')
+def amr():
+    return app.send_static_file('amr.html')
+
+
+@app.route('/conll')
+def amr():
+    return app.send_static_file('conll.html')
+
+
+if __name__ == "__main__":
+    app.run()
