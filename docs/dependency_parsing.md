963da766ddbb076c678fcbb066764b01c565d8db
anhv <anhv.ict91@gmail.com>
2020-09-27 13:48:16 +0700

    update

--- docs/dependency_parsing.md
+++ docs/dependency_parsing.md
@@ -2,3 +2,47 @@
 
 * `cmpobj`: Quan hệ so sánh
 
+### Xây dựng Treebank chuẩn CoNLLU
+
+Các bước xây dựng treebank chuẩn CoNLLU (1)
+
+Bước 1: Thống kê Treebank
+
+```
+perl tools/conllu-stats.pl TREE_BANK/*.conllu > stats.xml 
+```
+
+Với Treebank BKT
+
+Sinh ra file stats.xml
+
+```
+perl tools/conllu-stats.pl UD_Vietnamese-BKT/*.conllu > bkt_docs/stats.xml 
+```
+
+Sinh ra docs theo format `newdetailed`
+
+```
+cd tmp
+perl tools/conllu-stats.pl --oformat newdetailed --release 2.6 --treebank UD_Vietnamese-BKT --docs bkt_docs --data . --lang vi
+```
+
+So sánh 2 treebank 
+
+```
+cd tmp
+perl tools/conllu-stats.pl --oformat hubcompare UD_Vietnamese-BKT UD_Vietnamese-VTB  > comparison.md 
+```
+
+
+Bước 2: Kiểm tra TreeBank
+
+
+```
+python tools/validate.py --lang=vi VTB/vi_vtb-ud-train.conllu
+```
+
+
+Tham khảo
+
+(1): https://universaldependencies.org/release_checklist.html
--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
+import unicodedata
+
 from data import Corpus
 
 form_set = set()
 lemma_set = set()
+form_upos_set = set()
 upos_set = set()
 xpos_set = set()
 feats_set = set()
@@ -28,6 +31,7 @@ def load_bk_treebank():
             id, form, lemma, upos, xpos, feats, head, deprel, deps, misc = tokens
             form_set.add(form)
             lemma_set.add(lemma)
+            form_upos_set.add((form, upos))
             upos_set.add(upos)
             xpos_set.add(xpos)
             feats_set.add(feats)
@@ -41,4 +45,31 @@ def load_bk_treebank():
 sentences = load_bk_treebank()
 # print("== FORM == ")
 # print(form_set)
-print(sorted(deprel_set))
+
+# print(sorted(deprel_set))
+
+
+def write_form_upos_set(form_upos_set):
+    data = sorted(form_upos_set)
+    f = open("tmp/bk_form_upos.csv", "w")
+    f.write("form\tupos\n")
+    for form, upos in data:
+        f.write(f"{form}\t{upos}\n")
+
+
+def normalize_BKT():
+    content = open("tmp/BKT/train").read()
+    content = unicodedata.normalize("NFC", content)
+    open("tmp/UD_BKT/vi_bkt-ud-train.conllu", "w").write(content)
+
+    content = open("tmp/BKT/dev").read()
+    content = unicodedata.normalize("NFC", content)
+    open("tmp/UD_BKT/vi_bkt-ud-dev.conllu", "w").write(content)
+
+    content = open("tmp/BKT/test").read()
+    content = unicodedata.normalize("NFC", content)
+    open("tmp/UD_BKT/vi_bkt-ud-test.conllu", "w").write(content)
+
+# write_form_upos_set(form_upos_set)
+
+normalize_BKT()
