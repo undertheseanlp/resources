ed91c943ef5ef219baf6bc462716a89e6a1adb0e
Vu Anh <anhv.ict91@gmail.com>
2020-09-28 18:01:06 +0700

    update

---
+++ docs/pos_tag/DET.md
@@ -0,0 +1,8 @@
+# Định từ
+
+Nhãn UD: Determiner
+
+Tham khảo: 
+
+* (1) https://universaldependencies.org/u/pos/DET.html
+* (2) https://ngnnghc.wordpress.com/2011/03/25/phụ-từ-tiếng-việt/
--- utils.py
+++ utils.py
@@ -87,22 +87,19 @@ def convert_bkt_to_ud_sentence_converter(sentence, i, corpus_name):
     return result
 
 
-def convert_bkt_to_ud_generate_sent_id(content, corpus_name):
-    sentences = content.split("\n\n")
-    # sentences = [convert_bkt_to_ud_sentence_converter(sentence, i, corpus_name) for (i, sentence) in enumerate(sentences)]
-    content = "\n\n".join(sentences)
-    return content
-
-
-def convert_bkt_to_ud(content, corpus_name):
-    content = unicodedata.normalize("NFC", content)
-
+def convert_bkt_to_ud2(content, corpus_name):
     # tags
     content = content.replace("\tNN\t", "\tNOUN\t")
     content = content.replace("\tNNP\t", "\tPROPN\t")
     content = content.replace("\tCL\t", "\tNOUN\t")
     content = content.replace("\tRB\t", "\tADV\t")
+
     content = content.replace("\tVB\t", "\tVERB\t")
+    content = content.replace("\tAV\t", "\tVERB\t")
+
+    content = content.replace("\tJJ\t", "\tADJ\t")
+    content = content.replace("\tVA\t", "\tADJ\t")
+
     content = content.replace("\tIN\t", "\tADP\t")
     content = content.replace("\tMD\t", "\tAUX\t")
 
@@ -110,27 +107,59 @@ def convert_bkt_to_ud(content, corpus_name):
     content = content.replace("\tcl\t", "\tclf\t")
     content = content.replace("\tROOT\t", "\troot\t")
     content = content.replace("\tdobj\t", "\tobj\t")
+    return content
 
+
+def normalize_bkt_2():
+    SOURCE_TREEBANK = "UD_Vietnamese-BKT1"
+    DEST_TREEBANK = "UD_Vietnamese-BKT2"
+
+    content = open(f"tmp/{SOURCE_TREEBANK}/train").read()
+    content = convert_bkt_to_ud2(content, "train")
+    open(f"tmp/{DEST_TREEBANK}/vi_bkt2-ud-train.conllu", "w").write(content)
+
+    content = open(f"tmp/{SOURCE_TREEBANK}/dev").read()
+    content = convert_bkt_to_ud2(content, "dev")
+    open(f"tmp/{DEST_TREEBANK}/vi_bkt2-ud-dev.conllu", "w").write(content)
+
+    content = open(f"tmp/{SOURCE_TREEBANK}/test").read()
+    content = convert_bkt_to_ud2(content, "test")
+    open(f"tmp/{DEST_TREEBANK}/vi_bkt2-ud-test.conllu", "w").write(content)
+
+
+def convert_bkt_to_ud_generate_sent_id(content, corpus_name):
+    sentences = content.split("\n\n")
+    sentences = sentences[:-1]
+    sentences = [convert_bkt_to_ud_sentence_converter(sentence, i, corpus_name) for (i, sentence) in
+                 enumerate(sentences)]
+    content = "\n\n".join(sentences)
+    return content
+
+
+def convert_bkt_to_ud1(content, corpus_name):
+    content = unicodedata.normalize("NFC", content)
     content = convert_bkt_to_ud_generate_sent_id(content, corpus_name)
     return content
 
 
-def normalize_BKT_2():
-    UD_FOLDER = "UD_Vietnamese-BKT2"
-    content = open("tmp/BKT/train").read()
-    content = convert_bkt_to_ud(content, "train")
-    open(f"tmp/{UD_FOLDER}/vi_bkt2-ud-train.conllu", "w").write(content)
+def normalize_bkt_1():
+    SOURCE_FOLDER = "tmp/BKT"
+    UD_FOLDER = "UD_Vietnamese-BKT1"
+    content = open(f"{SOURCE_FOLDER}/train").read()
+    content = convert_bkt_to_ud1(content, "train")
+    open(f"tmp/{UD_FOLDER}/train", "w").write(content)
 
-    content = open("tmp/BKT/dev").read()
-    content = convert_bkt_to_ud(content, "dev")
-    open(f"tmp/{UD_FOLDER}/vi_bkt2-ud-dev.conllu", "w").write(content)
+    content = open(f"{SOURCE_FOLDER}/dev").read()
+    content = convert_bkt_to_ud1(content, "dev")
+    open(f"tmp/{UD_FOLDER}/dev", "w").write(content)
 
-    content = open("tmp/BKT/test").read()
-    content = convert_bkt_to_ud(content, "test")
-    open(f"tmp/{UD_FOLDER}/vi_bkt2-ud-test.conllu", "w").write(content)
+    content = open(f"{SOURCE_FOLDER}/test").read()
+    content = convert_bkt_to_ud1(content, "test")
+    open(f"tmp/{UD_FOLDER}/test", "w").write(content)
 
 
 # write_form_upos_set(form_upos_set)
 
 # normalize_BKT()
-normalize_BKT_2()
+normalize_bkt_1()
+# normalize_bkt_2()
