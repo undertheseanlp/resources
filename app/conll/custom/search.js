490b08050d506cd3b52fbc2657f26f3dea7e0de8
anhv <anhv.ict91@gmail.com>
2020-10-11 08:48:04 +0700

    update

--- app/conll/custom/search.js
+++ app/conll/custom/search.js
@@ -8,10 +8,46 @@ async function searchAPI(){
 
 }
 
+class Sentence {
+  constructor(domId, content) {
+    this.dom = "#" + domId;
+    this.articleDom = "#article-" + domId;
+    this.sentIdDom = this.articleDom + " g.sentnum text"
+    this.content = content;
+  }
+
+  hide(){
+    $(this.articleDom).hide()
+  }
+
+  updateSentIdDom(){
+    $(this.sentIdDom).html("abjaldjfa")
+  }
+  show(){
+    var content = this.content.replace("sent_id =", "sentence-label")
+    console.log(content)
+    $(this.dom).val(content)
+    $(this.dom).trigger("keyup")
+    setTimeout(this.updateSentIdDom, 5000)
+//    setTimeout(function(){
+//      $(this.sentIdDom).html("abjaldjfa")
+//    }, 1000)
+  }
+}
+
 function search(){
   searchAPI().then(function(data){
-    console.log(data);
+  console.log("search")
+    content = data["sent"] + "\n"
+    sentence = new Sentence("input", content)
+    sentence.show()
+
+    sentence2 = new Sentence("input-2", content)
+    sentence2.show()
+//    $("#input").val(content)
+//    $("#input").trigger("keyup");
   })
 }
 
-search();
+//setTimeout(search, 2000);
+//search()
