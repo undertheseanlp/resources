23aab751ccdb72b9be9006ac1118fb16da615786
Vu Anh <anhv.ict91@gmail.com>
2020-11-06 15:28:31 +0700

    update

--- tools/dicitonary.py
+++ tools/dicitonary.py
@@ -5,4 +5,3 @@ root = dirname(dirname(__file__))
 dictionary_file = join(root, "data", "dictionary", "data.yaml")
 with open(dictionary_file) as f:
     data = yaml.safe_load(open(dictionary_file))
-print(0)
