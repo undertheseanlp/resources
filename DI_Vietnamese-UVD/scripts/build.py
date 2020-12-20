0cd970ed0d539746235f018ab387da1186a3baa4
anhv <anhv.ict91@gmail.com>
2020-12-20 21:32:23 +0700

    update

--- DI_Vietnamese-UVD/scripts/build.py
+++ DI_Vietnamese-UVD/scripts/build.py
@@ -1,29 +1,28 @@
 import yaml
-import os
-from os.path import join, basename
+from os.path import join, basename, dirname
 
 from termcolor import colored
 
-total_erros = 0
-SCRIPTS_FOLDER = os.path.dirname(__file__)
-DATA_FOLDER = join(os.path.dirname(SCRIPTS_FOLDER), "data")
+total_errors = 0
+SCRIPTS_FOLDER = dirname(__file__)
+DICT_FOLDER = join(dirname(SCRIPTS_FOLDER), 'dictionary')
 
 
 def warn(file, line_number, message, type=None):
-    global total_erros
+    global total_errors
     text = ""
     if type:
         text = f"[{type}] "
     text += basename(file) + ":" + str(line_number)
     print(colored(text, 'red'), colored(message, 'red'))
 
-    total_erros += 1
+    total_errors += 1
 
 
 MIN_WORDS_IN_DICT = 3000
 
-dictionary_file = join(DATA_FOLDER, "underthesea_dictionary.yaml")
-# dictionary_file = join(DATA_FOLDER, "data.yaml")
+# dictionary_file = join(DATA_FOLDER, "underthesea_dictionary.yaml")
+dictionary_file = join(DICT_FOLDER, "data.yaml")
 # validate
 with open(dictionary_file) as f:
     data = yaml.safe_load(f)
@@ -31,8 +30,8 @@ with open(dictionary_file) as f:
     if num_words < MIN_WORDS_IN_DICT:
         warn('DICT', '', f'Dictionary must has at least {MIN_WORDS_IN_DICT} words (found {num_words})')
 
-if total_erros > 0:
-    print(colored(f"\n[x] BUILD ERRORS: {total_erros} errors", 'red'))
+if total_errors > 0:
+    print(colored(f"\n[x] BUILD ERRORS: {total_errors} errors", 'red'))
 else:
     print(f"\n[+] BUILD SUCCESS")
 
