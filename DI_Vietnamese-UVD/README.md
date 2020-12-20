53bfbd166ed43deb878c4965be6f472d5d678f85
anhv <anhv.ict91@gmail.com>
2020-12-20 11:04:24 +0700

    update

--- DI_Vietnamese-UVD/README.md
+++ DI_Vietnamese-UVD/README.md
@@ -1,3 +1,20 @@
 # DI-Vietnamese-UVD
 
 Underthesea Vietnamese Dictionary
+
+# ERRORS CODE
+
+<table>
+<tr>
+<th>Code</th>
+<th>Example Message</th>
+</tr>
+<tr>
+<td><b>E1xx</b></td>
+<td><i>Dictionary Error</i></td>
+</tr>
+<tr>
+<td>E101</td>
+<td>Dictionary must has at least N words (found M)</td>
+</tr>
+</table>
---
+++ DI_Vietnamese-UVD/scripts/build.py
@@ -0,0 +1,44 @@
+import yaml
+import os
+from os.path import join, basename
+
+from termcolor import colored
+
+total_erros = 0
+SCRIPTS_FOLDER = os.path.dirname(__file__)
+DATA_FOLDER = join(os.path.dirname(SCRIPTS_FOLDER), "data")
+
+
+def warn(file, line_number, message, type=None):
+    global total_erros
+    text = ""
+    if type:
+        text = f"[{type}] "
+    text += basename(file) + ":" + str(line_number)
+    print(colored(text, 'red'), colored(message, 'red'))
+
+    total_erros += 1
+
+
+MIN_WORDS_IN_DICT = 3000
+
+# validate
+with open(join(DATA_FOLDER, "data.yaml")) as f:
+    data = yaml.safe_load(f)
+    num_words = len(data)
+    if num_words < MIN_WORDS_IN_DICT:
+        warn('DICT', '', f'Dictionary must has at least {MIN_WORDS_IN_DICT} words (found {num_words})')
+
+if total_erros > 0:
+    print(colored(f"\n[x] BUILD ERRORS: {total_erros} errors", 'red'))
+else:
+    print(f"\n[+] BUILD SUCCESS")
+
+
+def stats():
+    print("\n# DICTIONARY STATISTICS")
+    global num_words
+    print("Number words:", num_words)
+
+
+stats()
