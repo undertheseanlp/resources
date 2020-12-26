c6ed7e34f45bb3178144fc96c5d3e95a87ea2fcc
anhv <anhv.ict91@gmail.com>
2020-12-26 10:36:13 +0700

    update

--- release.py
+++ release.py
@@ -15,7 +15,7 @@ for release in releases:
     print(release)
 
 print(G.get_user())
-print(G.get_user().get_repos())
+print(len(G.get_user().get_repos()))
 # for repo in G.get_user().get_repos():
 #     print(repo.name)
 
