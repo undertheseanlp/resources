71d33bbf4c03226bd97be9c74d6708ac213932e6
anhv <anhv.ict91@gmail.com>
2020-12-26 16:13:10 +0700

    GH-40

--- automatic_build.py
+++ automatic_build.py
@@ -1,1 +1,9 @@
 print("hihi")
+import os
+from github import Github
+
+G = Github(os.environ['GITHUB_TOKEN'])
+repo = G.get_repo("undertheseanlp/resources")
+
+repo.create_file("new_file.txt", "init commit", "file_content ------ ")
+print('hihi')
