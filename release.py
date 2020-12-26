ff8b2b58fad92aa5b6185a0de4f853139f5763c1
Vu Anh <anhv.ict91@gmail.com>
2020-12-26 09:47:21 +0700

    update

--- release.py
+++ release.py
@@ -6,3 +6,9 @@ print("Get Release")
 from github import Github
 token = os.getenv('GITHUB_TOKEN', '...')
 print(token)
+G = Github(token)
+
+repo = G.get_repo("undertheseanlp/resources")
+releases = repo.get_releases()
+for release in releases:
+    print(release)
