e308256555636ab61305e5dbf568ab37848f9d56
Vu Anh <anhv.ict91@gmail.com>
2020-12-26 10:53:01 +0700

    update

--- release.py
+++ release.py
@@ -1,7 +1,7 @@
 import os
 import sys
 print("Release")
-
+version = open("VERSION").read().strip()
 from github import Github
 token = os.environ['GITHUB_TOKEN']
 print("environ", os.environ)
@@ -10,7 +10,7 @@ print(sys.argv)
 G = Github(token)
 
 repo = G.get_repo("undertheseanlp/resources")
-repo.create_git_release('ghi', 'ghi', 'ghi')
+repo.create_git_release(version, f"Release {version}")
 
 
 
